600a3897a629a56b65ccba3161f3b3079ae592b1
Thomas Fiedler <t.fiedler@fio.de>
2015-05-08 09:35:08 +0200

    Add additional project to config. Add Links to Bugzilla bugs in generated Markdown. Adjust off by 1 error  for week number extraction.

--- generator/reports/week17_weekly_report_VacationPlanner.md
+++
@@ -1,7 +0,0 @@
-##r155459 | t.fiedler | 2015-04-29 16:17:50 +0200 (Wed, 29 Apr 2015) | 3 lines
-Bug 57640 - Die E-Mails für Urlaubseintrag beinhalten den falschen Absender
-
-- Removed unused class NewSession.cs
-- Fixed an issue where the MailDistributor saved the sender of the mails and incorrectly used that for the next mail
-
-----------
--- generator/weekly.rb
+++ generator/weekly.rb
@@ -15,13 +15,15 @@ class Weekly
 		@user = c['user']
 		@repos = c['repos'].split(' ')
 		
-		@week = "week#{Date.parse('Sunday').strftime('%U')}"
+		@week = "week#{Date.parse('Sunday').strftime('%U').to_i + 1}"
+		puts @week
 		# initialize post
 		projects = @repos.collect do |r| r.split('/').last end
 		puts "projects: #{projects}"
 		@post = Post.new(@week, projects, '')
 		@dropbox = c['dropbox']
 		@posts_directory = c['posts_directory']
+		@bugzilla_url = c['bugzilla']
 	end
 	
 	def find_revisions(in_file)
@@ -60,49 +62,37 @@ class Weekly
 	def read_log(path_to_repo)
 		project_name = path_to_repo.split('/').last
 		# getting past revisions
-		target_path = "reports/week#{@week}_weekly_report_#{project_name}.md"
-		@revisions = find_revisions(target_path)
-		
+		target_path = "reports/#{@week}/#{project_name}.md"
+	
 		date = Date.parse("Sunday").strftime("%Y-%m-%d")
 		svn_cmd = "svn log #{path_to_repo} -v -r{#{date}}:HEAD --search #{@user} | grep - > weekly"
 	
 		r = %x[#{svn_cmd}]
-	
-		@post.content.concat "##{project_name}\n\n"
-		target = File.open(target_path, 'a')
+		
 		lines = get_lines(File.open('weekly'))
-		rem = 0
-		any_change = false
-		while rem < lines.length
-			l = lines[rem]
+		@post.content.concat "##{project_name}\n" unless lines.empty?
+		text = ''
+		lines.each do |l|
 			r = is_revision? l
-			skip = check_revision l
-			if skip == 0
-				puts "skipping #{skip}"
-				if r
-					# revision as headline
-					headline = "#####{l}" 
-					puts headline
-					target.write headline
-					@post.content.concat(headline)
-				elsif l.start_with? 'Bug'
-						puts l + "\n"
-						target.write l + "\n"
-						@post.content.concat(l+"\n")
-				else 
-						puts l
-						target.write l
-						@post.content.concat l
-				end
-				rem += 1
-				any_change = true
+			if r
+				# revision as headline
+				headline = "\n#####SVN Revision: #{l.split('|')[0]}\n" 
+				puts headline
+				text.concat(headline)
+			elsif l.start_with? 'Bug'
+				bug = l.gsub(/(Bug )([0-9]+)/, "[" + '\0'+ "]" + "(#{@bugzilla_url}" + '\2' + ")").concat "\n"		
+				puts bug	
+				text.concat(bug)
 			else 
-				rem += skip
-			end
-		end
-			 
-		target.write "\n----------\n" if any_change
-		target.close
+				puts l
+				text.concat l
+				end
+			end	 
+		
+		text.concat "\n"
+		@post.content.concat text
+		Dir.mkdir("reports/#{@week}") unless File.exists?("reports/#{@week}")
+		File.write(target_path, text)
 	end
 
 	def write_post
