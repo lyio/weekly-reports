78164864516a5622e4af711aa4801d720902340b
Thomas Fiedler <thfied@gmail.com>
2015-05-29 12:26:26 +0200

    #4 Add links to the commit and referenced issue to generated markdown. Extract the base_urls from 'origin'

--- generator/log-readers/git-log-reader.rb
+++ generator/log-readers/git-log-reader.rb
@@ -15,16 +15,20 @@ class GitLogReader < LogReader
 		
 		text = ''
 		commits.each do |commit|
+			# getting the remote url for origin to construct an issue link
+			url = repo.remotes.select { |r| r.name == 'origin' }.first.url.gsub('.git', '')
+			
 			#Commit hash as headline
-			headline = "\n#####Git commit: #{commit.sha}\n"
+			commit_url = url + "/commit/#{commit.sha})"
+			headline = "\n#####Git commit: [#{commit.sha}](#{commit_url}\n"
 			puts headline
 			text.concat headline
 			
 			# getting the issue information
-			#TODO: link issue to Gitlab 
 			issue = commit.message.split.select { |s| s.start_with? '#' }.first
 			puts issue
-			text.concat "Issue #{issue} - #{commit.name} \n\n"
+			issue_url = url.concat "/issues/#{issue.gsub('#', '')}"
+			text.concat "[Issue #{issue}](#{issue_url}) - #{commit.name} \n\n"
 			
 			# adding the rest of the message
 			text.concat commit.message.gsub('#', '\\#')
